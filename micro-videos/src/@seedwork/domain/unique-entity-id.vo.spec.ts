c41351049f9aac0007a6ba06c7f59bb3e4934bd2
Pedro Godoy <pedrohenrique.godoy@outlook.com>
2022-04-06 18:56:28 -0300

    feat: add category

---
+++ micro-videos/src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -0,0 +1,8 @@
+import InvalidUuidError from '../errors/invalid-uuid.error';
+import UniqueEntityId from './unique-entity-id.vo';
+
+describe('UniqueEntityId Unit Tests', () => {
+  it('should throw error when uuid is invalid', () => {
+    expect(() => new UniqueEntityId('invalid-uuid')).toThrow(new InvalidUuidError());
+  });
+});
